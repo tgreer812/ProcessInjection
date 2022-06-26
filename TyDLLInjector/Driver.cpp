77ea516fb81c8e8383a14fadb6cd925ce7849f8d
Tyler Greer <tgreer812@gmail.com>
2022-06-26 14:31:22 -0400

    Fixed dll injector
    Moved DLL injector to proper location and pushed

---
+++ TyDLLInjector/Driver.cpp
@@ -0,0 +1,33 @@
+/**/
+
+#include "stdafx.h"
+
+using namespace std;
+
+void printUsage()
+{
+    const char* usageString = "usage:\tTyDllInjector.exe TARGET_PROCESS INJECT_ME\n\n"
+        "\tpositional arguments:\n"
+        "\tTARGET_PROCESS\t\t\tTarget PID to inject into\n"
+        "\tINJECT_ME\t\t\tPath to DLL to inject\n\n";
+
+    printf("%s", usageString);
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        printUsage();
+        return;
+    }
+
+    std::string target_pid_str(argv[1]);
+    int target_pid = std::stoi(target_pid_str);
+
+    std::string injectee(argv[2]);
+
+
+
+}
+
---
+++ TyDLLInjector/stdafx.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <Windows.h>
+#include <stdio.h>
+#include <tchar.h>
+#include <iostream>
+#include <string>
+#include <iomanip>
+//#include <utility.h>
